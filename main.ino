ee99140510664f7817e4485fb74702331eb07fb9
dqh999 <dqhit999@gmail.com>
2025-02-19 15:40:15 +0700

    refactor code

--- main.ino
+++ main.ino
@@ -1,7 +1,7 @@
 #include "config/Constants.h"
 #include "modules/motor_control/motor_control.h"
 #include "modules/obstacle_sensor/obstacle_sensor.h"
-#include "modules/obstacle_avoidance/obstacle_avoidance.h"
+#include "obstacle_avoidance.h"
 
 MotorControl motor(MOTOR1_PIN1, MOTOR1_PIN2, MOTOR2_PIN1, MOTOR2_PIN2, MOTOR_SPEED_PIN1, MOTOR_SPEED_PIN2);
 ObstacleSensor sensor(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
