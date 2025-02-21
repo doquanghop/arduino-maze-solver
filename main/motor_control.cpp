db2fa97d02356a40aecef00b2e9eacaf64e29f78
Luna8125 <nguyenthuhuong08122005@gmail.com>
2025-02-21 17:13:30 +0700

    cập nhật hàm moveForward, backWard, moveLeft, moveRight, moveStop trong motor_control.cpp, thêm hàm trong rẽ trái, rẽ phải, dừng trong motor_control.h

--- main/motor_control.cpp
+++ main/motor_control.cpp
@@ -2,12 +2,67 @@
 
 MotorControl::MotorControl(int m1p1, int m1p2, int m2p1, int m2p2, int sp1, int sp2) {
     // TODO: Khởi tạo chân động cơ
+
+    // Gán giá trị chân điều khiển
+    this->motor1Pin1 = m1p1;
+    this->motor1Pin2 = m1p2;
+    this->motor2Pin1 = m2p1;
+    this->motor2Pin2 = m2p2;
+    this->motorSpeedPin1 = sp1;
+    this->motorSpeedPin2 = sp2;
+
+    // Cấu hình chân xuất tín hiệu
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(motor2Pin1, OUTPUT);
+    pinMode(motor2Pin2, OUTPUT);
+    pinMode(motorSpeedPin1, OUTPUT);
+    pinMode(motorSpeedPin2, OUTPUT);
+
+
 }
 
 void MotorControl::moveForward(int speed) {
     // TODO: Hàm điều khiển tiến
+
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+    analogWrite(motorSpeedPin1, speed);
+    analogWrite(motorSpeedPin2, speed);  
+
+
 }
 
 void MotorControl::moveBackward(int speed) {
     // TODO: Hàm điều khiển lùi
+
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, HIGH);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, HIGH);
+    analogWrite(motorSpeedPin1, speed);
+    analogWrite(motorSpeedPin2, speed);
+}
+
+void MotorControl::moveLeft(int speed) {
+  // Hàm điều khiển rẽ trái
+    
+    analogWrite(motorSpeedPin1, 0);  // dừng bánh trái
+    analogWrite(motorSpeedPin2, speed);
+}
+
+void MotorControl::moveRight(int speed) {
+  // Hàm điều khiển rẽ phải
+    
+    analogWrite(motorSpeedPin1, speed);  // dừng bánh phải
+    analogWrite(motorSpeedPin2, 0);
+}
+
+void MotorControl::stop() {
+  // hàm điều khiển dừng
+  // dừng cả 2 bánh
+    moveForward(0)
+
 }
