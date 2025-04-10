58c638f2417704149f1725370288e9459fe51ec8
dqh999 <dqhit999@gmail.com>
2025-04-10 13:02:53 +0700

    #32 done

--- main/obstacle_avoidance.cpp
+++ main/obstacle_avoidance.cpp
@@ -6,36 +6,6 @@ ObstacleAvoidance::ObstacleAvoidance(MotorControl* m, ObstacleSensor* s) {
 }
 
 void ObstacleAvoidance::checkAndAvoid() {
-    int distanceFront = sensor->getDistance();
-    delay(50);
-
-    if (distanceFront > 150) {
-      motor->moveForward(180);
-    } else if (distanceFront >= 20) {
-      motor->moveForward(150);
-    } else {
-      motor->moveStop();
-      delay(300);
-
-      motor->moveBackward(120);
-      delay(500);
-
-      motor->turnRight(150);
-
-      int distanceRight = sensor->getDistance();
-      if (distanceRight > 20) {
-        return;
-      }
-
-      motor->rotate180(150);
-      int distanceAfterRotate = sensor->getDistance();
-      if (distanceAfterRotate > 20) {
-        return;
-      }
-
-      motor->moveBackward(120);
-      delay(500);
-      motor->moveStop();
     }
 }
 
